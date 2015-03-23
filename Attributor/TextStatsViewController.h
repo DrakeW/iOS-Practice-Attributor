ef0f4552c3936d807ff3b69dc82db28c3d297317
wsupWorld <junyuw@berkeley.edu>
2015-03-23 14:33:33 -0700

    add stats analysis page and navigation controller

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Junyu Wang on 3/23/15.
+//  Copyright (c) 2015 Junyu Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
